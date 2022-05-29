c79feed2d9e39349dcdffb0d09b2b9de0cb2c192
Jose Soto <jsoto7087@gmail.com>
2022-05-28 21:40:48 -0400

    commit tree 3597e1828b86b07fbb6507b30056f4c6d3401231

---
+++ index.js
@@ -0,0 +1,36 @@
+function divide(divide = 2000, divisor = 100) {
+    return divide / divisor;
+}
+
+const square = (a) => {
+    return a * a;
+}
+
+const add = (a, b) => {
+    return a + b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function divide(divident = 2000, divisor = 100) {
+//     return (divident / divisor);
+// }
+
+// const square = (a) => {
+//     return (a * a);
+// }
+
+// const add = (a, b) => {
+//     return (a + b);
+// }
